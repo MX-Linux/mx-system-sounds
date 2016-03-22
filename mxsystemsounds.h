acb011cf393ac7048257e4afb24fe3ccbd4ae5fa
dolphin_oracle <dolphinoracle@gmail.com>
2016-03-22 12:42:37 -0400

    added all working checkboxes, changed cancel to close

--- mxsystemsounds.h
+++ mxsystemsounds.h
@@ -57,8 +57,9 @@ private slots:
     void on_buttonAbout_clicked();
     void on_buttonHelp_clicked();
 
-    //void on_pushButton_playlogoutsound_clicked();
-    //void on_checkbox_eventsounds_clicked();
+    void on_pushButton_playlogoutsound_clicked();
+
+    void on_checkbox_eventsounds_clicked();
 
 private:
     Ui::mxsystemsounds *ui;
