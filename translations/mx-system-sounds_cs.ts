be7dab8c665e501a9c9bd4f81eb5c16e7c817aa8
Adrian <adrian@mxlinux.org>
2018-10-14 20:26:23 -0400

    new build

--- translations/mx-system-sounds_cs.ts
+++ translations/mx-system-sounds_cs.ts
@@ -1,190 +1,192 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="cs" version="2.1">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs">
 <context>
     <name>mxsystemsounds</name>
     <message>
-        <location filename="mxsystemsounds.ui" line="14"/>
-        <location filename="mxsystemsounds.cpp" line="70"/>
-        <location filename="mxsystemsounds.cpp" line="288"/>
-        <location filename="mxsystemsounds.cpp" line="300"/>
-        <location filename="mxsystemsounds.cpp" line="334"/>
+        <location filename="../mxsystemsounds.ui" line="14"/>
+        <location filename="../mxsystemsounds.cpp" line="70"/>
+        <location filename="../mxsystemsounds.cpp" line="288"/>
+        <location filename="../mxsystemsounds.cpp" line="300"/>
+        <location filename="../mxsystemsounds.cpp" line="334"/>
         <source>MX System Sounds</source>
         <translation>MX Systémové zvuky</translation>
     </message>
     <message>
-        <location filename="mxsystemsounds.ui" line="48"/>
+        <location filename="../mxsystemsounds.ui" line="48"/>
         <source>Event Sounds</source>
         <translation>Zvuky událostí</translation>
     </message>
     <message>
-        <location filename="mxsystemsounds.ui" line="61"/>
+        <location filename="../mxsystemsounds.ui" line="61"/>
         <source>Session Sounds</source>
         <translation>Zvuky relace</translation>
     </message>
     <message>
-        <location filename="mxsystemsounds.ui" line="68"/>
+        <location filename="../mxsystemsounds.ui" line="68"/>
         <source>XFCE Event Sounds</source>
         <translation>Zvuky událostí XFCE</translation>
     </message>
     <message>
-        <location filename="mxsystemsounds.ui" line="75"/>
+        <location filename="../mxsystemsounds.ui" line="75"/>
         <source>Login</source>
         <translation>Příhlášení</translation>
     </message>
     <message>
-        <location filename="mxsystemsounds.ui" line="82"/>
+        <location filename="../mxsystemsounds.ui" line="82"/>
         <source>XFCE Input Feedback Sounds</source>
         <translation>Zvuky zpětné vazby XFCE</translation>
     </message>
     <message>
-        <location filename="mxsystemsounds.ui" line="89"/>
+        <location filename="../mxsystemsounds.ui" line="89"/>
         <source>Logout</source>
         <translation>Odhlášení</translation>
     </message>
     <message>
-        <location filename="mxsystemsounds.ui" line="118"/>
+        <location filename="../mxsystemsounds.ui" line="118"/>
         <source>Custom Sounds</source>
         <translation>Vlastní zvuky</translation>
     </message>
     <message>
-        <location filename="mxsystemsounds.ui" line="125"/>
+        <location filename="../mxsystemsounds.ui" line="125"/>
         <source>Theme</source>
         <translation>Motiv</translation>
     </message>
     <message>
-        <location filename="mxsystemsounds.ui" line="132"/>
-        <location filename="mxsystemsounds.ui" line="153"/>
+        <location filename="../mxsystemsounds.ui" line="132"/>
+        <location filename="../mxsystemsounds.ui" line="153"/>
         <source>Login Sound</source>
         <translation>Zvuk při přihlášení</translation>
     </message>
     <message>
-        <location filename="mxsystemsounds.ui" line="139"/>
-        <location filename="mxsystemsounds.ui" line="185"/>
+        <location filename="../mxsystemsounds.ui" line="139"/>
+        <location filename="../mxsystemsounds.ui" line="185"/>
         <source>...</source>
         <translation>...</translation>
     </message>
     <message>
-        <location filename="mxsystemsounds.ui" line="164"/>
-        <location filename="mxsystemsounds.ui" line="207"/>
-        <location filename="mxsystemsounds.ui" line="210"/>
+        <location filename="../mxsystemsounds.ui" line="164"/>
+        <location filename="../mxsystemsounds.ui" line="207"/>
+        <location filename="../mxsystemsounds.ui" line="210"/>
         <source>Theme Default</source>
         <translation>Předvolený motiv</translation>
     </message>
     <message>
-        <location filename="mxsystemsounds.ui" line="167"/>
+        <location filename="../mxsystemsounds.ui" line="167"/>
         <source>Use Default</source>
         <translation>Použít předvolený</translation>
     </message>
     <message>
-        <location filename="mxsystemsounds.ui" line="178"/>
-        <location filename="mxsystemsounds.ui" line="196"/>
+        <location filename="../mxsystemsounds.ui" line="178"/>
+        <location filename="../mxsystemsounds.ui" line="196"/>
         <source>Logout Sound</source>
         <translation>Zvuk při odhlášení</translation>
     </message>
     <message>
-        <location filename="mxsystemsounds.ui" line="229"/>
+        <location filename="../mxsystemsounds.ui" line="229"/>
         <source>Borealis</source>
         <translation>Borealis</translation>
     </message>
     <message>
-        <location filename="mxsystemsounds.ui" line="260"/>
+        <location filename="../mxsystemsounds.ui" line="260"/>
         <source>About this application</source>
         <translation>O této aplikaci</translation>
     </message>
     <message>
-        <location filename="mxsystemsounds.ui" line="263"/>
+        <location filename="../mxsystemsounds.ui" line="263"/>
         <source>About...</source>
         <translation>O programu</translation>
     </message>
     <message>
-        <location filename="mxsystemsounds.ui" line="270"/>
+        <location filename="../mxsystemsounds.ui" line="270"/>
         <source>Alt+B</source>
         <translation>Alt+B</translation>
     </message>
     <message>
-        <location filename="mxsystemsounds.ui" line="286"/>
+        <location filename="../mxsystemsounds.ui" line="286"/>
         <source>Display help </source>
         <translation>Zobrazit nápovědu</translation>
     </message>
     <message>
-        <location filename="mxsystemsounds.ui" line="289"/>
+        <location filename="../mxsystemsounds.ui" line="289"/>
         <source>Help</source>
         <translation>Nápověda</translation>
     </message>
     <message>
-        <location filename="mxsystemsounds.ui" line="296"/>
+        <location filename="../mxsystemsounds.ui" line="296"/>
         <source>Alt+H</source>
         <translation>Alt+H</translation>
     </message>
     <message>
-        <location filename="mxsystemsounds.ui" line="354"/>
+        <location filename="../mxsystemsounds.ui" line="354"/>
         <source>Apply</source>
         <translation>Použít</translation>
     </message>
     <message>
-        <location filename="mxsystemsounds.ui" line="380"/>
+        <location filename="../mxsystemsounds.ui" line="380"/>
         <source>Quit application</source>
         <translation>Ukončit aplikaci</translation>
     </message>
     <message>
-        <location filename="mxsystemsounds.ui" line="383"/>
+        <location filename="../mxsystemsounds.ui" line="383"/>
         <source>Close</source>
         <translation>Zavřít</translation>
     </message>
     <message>
-        <location filename="mxsystemsounds.ui" line="390"/>
+        <location filename="../mxsystemsounds.ui" line="390"/>
         <source>Alt+N</source>
         <translation>Alt+N</translation>
     </message>
     <message>
-        <location filename="mxsystemsounds.cpp" line="287"/>
+        <location filename="../mxsystemsounds.cpp" line="287"/>
         <source>About MX System Sounds</source>
         <translation>O programu MX Systémové zvuky</translation>
     </message>
     <message>
-        <location filename="mxsystemsounds.cpp" line="288"/>
+        <location filename="../mxsystemsounds.cpp" line="288"/>
         <source>Version: </source>
         <translation>Verze:</translation>
     </message>
     <message>
-        <location filename="mxsystemsounds.cpp" line="289"/>
+        <location filename="../mxsystemsounds.cpp" line="289"/>
         <source>Configure Event &amp; Session Sounds</source>
         <translation>Konfigurace zvuků událostí a relace</translation>
     </message>
     <message>
-        <location filename="mxsystemsounds.cpp" line="291"/>
+        <location filename="../mxsystemsounds.cpp" line="291"/>
         <source>Copyright (c) MX Linux</source>
         <translation>Vlastnická práva (c) MX Linux</translation>
     </message>
     <message>
-        <location filename="mxsystemsounds.cpp" line="292"/>
-        <location filename="mxsystemsounds.cpp" line="300"/>
+        <location filename="../mxsystemsounds.cpp" line="292"/>
+        <location filename="../mxsystemsounds.cpp" line="300"/>
         <source>License</source>
         <translation>Licence</translation>
     </message>
     <message>
-        <location filename="mxsystemsounds.cpp" line="293"/>
+        <location filename="../mxsystemsounds.cpp" line="293"/>
         <source>Changelog</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mxsystemsounds.cpp" line="294"/>
+        <location filename="../mxsystemsounds.cpp" line="294"/>
         <source>Cancel</source>
         <translation>Zrušit</translation>
     </message>
     <message>
-        <location filename="mxsystemsounds.cpp" line="309"/>
+        <location filename="../mxsystemsounds.cpp" line="309"/>
         <source>&amp;Close</source>
         <translation>&amp;Zavřít</translation>
     </message>
     <message>
-        <location filename="mxsystemsounds.cpp" line="353"/>
-        <location filename="mxsystemsounds.cpp" line="383"/>
+        <location filename="../mxsystemsounds.cpp" line="353"/>
+        <location filename="../mxsystemsounds.cpp" line="383"/>
         <source>Select Sound File</source>
         <translation>Vyberte zvukový soubor</translation>
     </message>
     <message>
-        <location filename="mxsystemsounds.cpp" line="353"/>
-        <location filename="mxsystemsounds.cpp" line="383"/>
+        <location filename="../mxsystemsounds.cpp" line="353"/>
+        <location filename="../mxsystemsounds.cpp" line="383"/>
         <source>Sound Files (*.mp3 *.m4a *.aac *.flac *.ogg *.oga *.wav)</source>
         <translation>Soubory zvuku (*.mp3 *.m4a *.aac *.flac *.ogg *.oga *.wav)</translation>
     </message>
