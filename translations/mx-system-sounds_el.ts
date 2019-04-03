09e50503fd2ad6628cbba21a499daea14b071beb
oldgizahub <geoffgigg@hotmail.com>
2019-04-03 08:55:51 -0400

    Translation refresh April 2019

--- translations/mx-system-sounds_el.ts
+++ translations/mx-system-sounds_el.ts
@@ -1,192 +1,190 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="el">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="el" version="2.1">
 <context>
     <name>mxsystemsounds</name>
     <message>
-        <location filename="../mxsystemsounds.ui" line="14"/>
-        <location filename="../mxsystemsounds.cpp" line="70"/>
-        <location filename="../mxsystemsounds.cpp" line="288"/>
-        <location filename="../mxsystemsounds.cpp" line="300"/>
-        <location filename="../mxsystemsounds.cpp" line="334"/>
+        <location filename="mxsystemsounds.ui" line="14"/>
+        <location filename="mxsystemsounds.cpp" line="70"/>
+        <location filename="mxsystemsounds.cpp" line="288"/>
+        <location filename="mxsystemsounds.cpp" line="300"/>
+        <location filename="mxsystemsounds.cpp" line="334"/>
         <source>MX System Sounds</source>
         <translation>MX Ήχοι Συστήματος</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="48"/>
+        <location filename="mxsystemsounds.ui" line="48"/>
         <source>Event Sounds</source>
         <translation>Ήχοι Συμβάντος</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="61"/>
+        <location filename="mxsystemsounds.ui" line="61"/>
         <source>Session Sounds</source>
         <translation>Ήχοι Συνεδρίας</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="68"/>
+        <location filename="mxsystemsounds.ui" line="68"/>
         <source>XFCE Event Sounds</source>
         <translation>XFCE Ήχοι Συμβάντος</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="75"/>
+        <location filename="mxsystemsounds.ui" line="75"/>
         <source>Login</source>
         <translation>Σύνδεση</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="82"/>
+        <location filename="mxsystemsounds.ui" line="82"/>
         <source>XFCE Input Feedback Sounds</source>
         <translation>XFCE Εισαγωγή Ανατροφοδότησης Ήχων</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="89"/>
+        <location filename="mxsystemsounds.ui" line="89"/>
         <source>Logout</source>
         <translation>Αποσύνδεση</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="118"/>
+        <location filename="mxsystemsounds.ui" line="118"/>
         <source>Custom Sounds</source>
         <translation>Συνήθεις Ήχοι</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="125"/>
+        <location filename="mxsystemsounds.ui" line="125"/>
         <source>Theme</source>
         <translation>Θέμα</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="132"/>
-        <location filename="../mxsystemsounds.ui" line="153"/>
+        <location filename="mxsystemsounds.ui" line="132"/>
+        <location filename="mxsystemsounds.ui" line="153"/>
         <source>Login Sound</source>
         <translation>Ήχος Σύνδεσης</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="139"/>
-        <location filename="../mxsystemsounds.ui" line="185"/>
+        <location filename="mxsystemsounds.ui" line="139"/>
+        <location filename="mxsystemsounds.ui" line="185"/>
         <source>...</source>
         <translation>...</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="164"/>
-        <location filename="../mxsystemsounds.ui" line="207"/>
-        <location filename="../mxsystemsounds.ui" line="210"/>
+        <location filename="mxsystemsounds.ui" line="164"/>
+        <location filename="mxsystemsounds.ui" line="207"/>
+        <location filename="mxsystemsounds.ui" line="210"/>
         <source>Theme Default</source>
         <translation>Προεπιλεγμένο Θέμα</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="167"/>
+        <location filename="mxsystemsounds.ui" line="167"/>
         <source>Use Default</source>
         <translation>Χρήση Προεπιλεγμένου</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="178"/>
-        <location filename="../mxsystemsounds.ui" line="196"/>
+        <location filename="mxsystemsounds.ui" line="178"/>
+        <location filename="mxsystemsounds.ui" line="196"/>
         <source>Logout Sound</source>
         <translation>Ήχος Αποσύνδεσης</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="229"/>
+        <location filename="mxsystemsounds.ui" line="229"/>
         <source>Borealis</source>
         <translation>Σέλας</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="260"/>
+        <location filename="mxsystemsounds.ui" line="260"/>
         <source>About this application</source>
         <translation>Περί εφαρμογής.</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="263"/>
+        <location filename="mxsystemsounds.ui" line="263"/>
         <source>About...</source>
         <translation>Περί</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="270"/>
+        <location filename="mxsystemsounds.ui" line="270"/>
         <source>Alt+B</source>
         <translation>Alt+B</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="286"/>
+        <location filename="mxsystemsounds.ui" line="286"/>
         <source>Display help </source>
         <translation>Δείτε Βοήθεια</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="289"/>
+        <location filename="mxsystemsounds.ui" line="289"/>
         <source>Help</source>
         <translation>Βοήθεια </translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="296"/>
+        <location filename="mxsystemsounds.ui" line="296"/>
         <source>Alt+H</source>
         <translation>Alt+H </translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="354"/>
+        <location filename="mxsystemsounds.ui" line="354"/>
         <source>Apply</source>
         <translation>Εφαρμογή</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="380"/>
+        <location filename="mxsystemsounds.ui" line="380"/>
         <source>Quit application</source>
         <translation>Κλείστε την εφαρμογή </translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="383"/>
+        <location filename="mxsystemsounds.ui" line="383"/>
         <source>Close</source>
         <translation>Κλείσιμο</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="390"/>
+        <location filename="mxsystemsounds.ui" line="390"/>
         <source>Alt+N</source>
         <translation>Alt+N</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.cpp" line="287"/>
+        <location filename="mxsystemsounds.cpp" line="287"/>
         <source>About MX System Sounds</source>
         <translation>Σχετικά με τους  MX Ήχους Συστήματος</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.cpp" line="288"/>
+        <location filename="mxsystemsounds.cpp" line="288"/>
         <source>Version: </source>
         <translation>Έκδοση:</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.cpp" line="289"/>
+        <location filename="mxsystemsounds.cpp" line="289"/>
         <source>Configure Event &amp; Session Sounds</source>
         <translation>Ρύθμιση Συμβάντος &amp; Ήχων Συνδεδρίας Συστήματος</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.cpp" line="291"/>
+        <location filename="mxsystemsounds.cpp" line="291"/>
         <source>Copyright (c) MX Linux</source>
         <translation>Copyright (c)  MX Linux </translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.cpp" line="292"/>
-        <location filename="../mxsystemsounds.cpp" line="300"/>
+        <location filename="mxsystemsounds.cpp" line="292"/>
+        <location filename="mxsystemsounds.cpp" line="300"/>
         <source>License</source>
         <translation>Άδεια</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.cpp" line="293"/>
+        <location filename="mxsystemsounds.cpp" line="293"/>
         <source>Changelog</source>
         <translation>Changelog</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.cpp" line="294"/>
+        <location filename="mxsystemsounds.cpp" line="294"/>
         <source>Cancel</source>
         <translation>Ακύρωση</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.cpp" line="309"/>
+        <location filename="mxsystemsounds.cpp" line="309"/>
         <source>&amp;Close</source>
         <translation>&amp;Κλείσιμο</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.cpp" line="353"/>
-        <location filename="../mxsystemsounds.cpp" line="383"/>
+        <location filename="mxsystemsounds.cpp" line="353"/>
+        <location filename="mxsystemsounds.cpp" line="383"/>
         <source>Select Sound File</source>
         <translation>Επιλογή Αρχείου Ήχου</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.cpp" line="353"/>
-        <location filename="../mxsystemsounds.cpp" line="383"/>
+        <location filename="mxsystemsounds.cpp" line="353"/>
+        <location filename="mxsystemsounds.cpp" line="383"/>
         <source>Sound Files (*.mp3 *.m4a *.aac *.flac *.ogg *.oga *.wav)</source>
         <translation>Αρχεία Ήχου (*.mp3 *.m4a *.aac *.flac *.ogg *.oga *.wav)</translation>
     </message>
--- translations/mx-system-sounds_tr.ts
+++ translations/mx-system-sounds_tr.ts
@@ -1,192 +1,190 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="tr">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="tr" version="2.1">
 <context>
     <name>mxsystemsounds</name>
     <message>
-        <location filename="../mxsystemsounds.ui" line="14"/>
-        <location filename="../mxsystemsounds.cpp" line="70"/>
-        <location filename="../mxsystemsounds.cpp" line="288"/>
-        <location filename="../mxsystemsounds.cpp" line="300"/>
-        <location filename="../mxsystemsounds.cpp" line="334"/>
+        <location filename="mxsystemsounds.ui" line="14"/>
+        <location filename="mxsystemsounds.cpp" line="70"/>
+        <location filename="mxsystemsounds.cpp" line="288"/>
+        <location filename="mxsystemsounds.cpp" line="300"/>
+        <location filename="mxsystemsounds.cpp" line="334"/>
         <source>MX System Sounds</source>
         <translation>MX Sistem Sesleri</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="48"/>
+        <location filename="mxsystemsounds.ui" line="48"/>
         <source>Event Sounds</source>
         <translation>Olay Sesleri</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="61"/>
+        <location filename="mxsystemsounds.ui" line="61"/>
         <source>Session Sounds</source>
         <translation>Oturum Sesleri</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="68"/>
+        <location filename="mxsystemsounds.ui" line="68"/>
         <source>XFCE Event Sounds</source>
         <translation>XFCE Olay Sesleri</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="75"/>
+        <location filename="mxsystemsounds.ui" line="75"/>
         <source>Login</source>
         <translation>Giriş</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="82"/>
+        <location filename="mxsystemsounds.ui" line="82"/>
         <source>XFCE Input Feedback Sounds</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="89"/>
+        <location filename="mxsystemsounds.ui" line="89"/>
         <source>Logout</source>
         <translation>Oturumu kapat</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="118"/>
+        <location filename="mxsystemsounds.ui" line="118"/>
         <source>Custom Sounds</source>
         <translation>Özel Sesler</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="125"/>
+        <location filename="mxsystemsounds.ui" line="125"/>
         <source>Theme</source>
         <translation>Tema</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="132"/>
-        <location filename="../mxsystemsounds.ui" line="153"/>
+        <location filename="mxsystemsounds.ui" line="132"/>
+        <location filename="mxsystemsounds.ui" line="153"/>
         <source>Login Sound</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="139"/>
-        <location filename="../mxsystemsounds.ui" line="185"/>
+        <location filename="mxsystemsounds.ui" line="139"/>
+        <location filename="mxsystemsounds.ui" line="185"/>
         <source>...</source>
         <translation>...</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="164"/>
-        <location filename="../mxsystemsounds.ui" line="207"/>
-        <location filename="../mxsystemsounds.ui" line="210"/>
+        <location filename="mxsystemsounds.ui" line="164"/>
+        <location filename="mxsystemsounds.ui" line="207"/>
+        <location filename="mxsystemsounds.ui" line="210"/>
         <source>Theme Default</source>
         <translation>Öntanımlı Tema</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="167"/>
+        <location filename="mxsystemsounds.ui" line="167"/>
         <source>Use Default</source>
         <translation>Öntanımlı Kullan</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="178"/>
-        <location filename="../mxsystemsounds.ui" line="196"/>
+        <location filename="mxsystemsounds.ui" line="178"/>
+        <location filename="mxsystemsounds.ui" line="196"/>
         <source>Logout Sound</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="229"/>
+        <location filename="mxsystemsounds.ui" line="229"/>
         <source>Borealis</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="260"/>
+        <location filename="mxsystemsounds.ui" line="260"/>
         <source>About this application</source>
         <translation>Uygulama hakkında</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="263"/>
+        <location filename="mxsystemsounds.ui" line="263"/>
         <source>About...</source>
         <translation>Hakkında...</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="270"/>
+        <location filename="mxsystemsounds.ui" line="270"/>
         <source>Alt+B</source>
         <translation>Alt+B</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="286"/>
+        <location filename="mxsystemsounds.ui" line="286"/>
         <source>Display help </source>
         <translation>Yardımı görüntüle</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="289"/>
+        <location filename="mxsystemsounds.ui" line="289"/>
         <source>Help</source>
         <translation>Yardım</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="296"/>
+        <location filename="mxsystemsounds.ui" line="296"/>
         <source>Alt+H</source>
         <translation>Alt+H</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="354"/>
+        <location filename="mxsystemsounds.ui" line="354"/>
         <source>Apply</source>
         <translation>Uygula</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="380"/>
+        <location filename="mxsystemsounds.ui" line="380"/>
         <source>Quit application</source>
         <translation>Uygulamadan çık</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="383"/>
+        <location filename="mxsystemsounds.ui" line="383"/>
         <source>Close</source>
         <translation>Kapat</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.ui" line="390"/>
+        <location filename="mxsystemsounds.ui" line="390"/>
         <source>Alt+N</source>
         <translation>Alt+N</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.cpp" line="287"/>
+        <location filename="mxsystemsounds.cpp" line="287"/>
         <source>About MX System Sounds</source>
         <translation>MX Sistem Sesleri Hakkında</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.cpp" line="288"/>
+        <location filename="mxsystemsounds.cpp" line="288"/>
         <source>Version: </source>
         <translation>Sürüm</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.cpp" line="289"/>
+        <location filename="mxsystemsounds.cpp" line="289"/>
         <source>Configure Event &amp; Session Sounds</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
-        <location filename="../mxsystemsounds.cpp" line="291"/>
+        <location filename="mxsystemsounds.cpp" line="291"/>
         <source>Copyright (c) MX Linux</source>
         <translation>Copyright (c) MX Linux</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.cpp" line="292"/>
-        <location filename="../mxsystemsounds.cpp" line="300"/>
+        <location filename="mxsystemsounds.cpp" line="292"/>
+        <location filename="mxsystemsounds.cpp" line="300"/>
         <source>License</source>
         <translation>Ruhsat</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.cpp" line="293"/>
+        <location filename="mxsystemsounds.cpp" line="293"/>
         <source>Changelog</source>
         <translation>Değişim günlüğü</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.cpp" line="294"/>
+        <location filename="mxsystemsounds.cpp" line="294"/>
         <source>Cancel</source>
         <translation>İptal</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.cpp" line="309"/>
+        <location filename="mxsystemsounds.cpp" line="309"/>
         <source>&amp;Close</source>
         <translation>&amp;Kapat</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.cpp" line="353"/>
-        <location filename="../mxsystemsounds.cpp" line="383"/>
+        <location filename="mxsystemsounds.cpp" line="353"/>
+        <location filename="mxsystemsounds.cpp" line="383"/>
         <source>Select Sound File</source>
         <translation>Ses Dosyası Seçimi</translation>
     </message>
     <message>
-        <location filename="../mxsystemsounds.cpp" line="353"/>
-        <location filename="../mxsystemsounds.cpp" line="383"/>
+        <location filename="mxsystemsounds.cpp" line="353"/>
+        <location filename="mxsystemsounds.cpp" line="383"/>
         <source>Sound Files (*.mp3 *.m4a *.aac *.flac *.ogg *.oga *.wav)</source>
         <translation>Ses Dosyaları (*.mp3 *.m4a *.aac *.flac *.ogg *.oga *.wav)</translation>
     </message>
