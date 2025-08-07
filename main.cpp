22d6d3d3a6658ba9dcb9e630b76d01d4f24593c5
Adrian <adrian@mxlinux.org>
2025-08-07 15:10:15 -0400

    Set Qt platform to XCB if not set

--- main.cpp
+++ main.cpp
@@ -34,6 +34,13 @@
 
 int main(int argc, char *argv[])
 {
+    // Set Qt platform to XCB (X11) if not already set and we're in X11 environment
+    if (qEnvironmentVariableIsEmpty("QT_QPA_PLATFORM")) {
+        if (!qEnvironmentVariableIsEmpty("DISPLAY")) {
+            qputenv("QT_QPA_PLATFORM", "xcb");
+        }
+    }
+
     QApplication a(argc, argv);
     QApplication::setWindowIcon(QIcon("/usr/share/pixmaps/mx-system-sounds.png"));
 
